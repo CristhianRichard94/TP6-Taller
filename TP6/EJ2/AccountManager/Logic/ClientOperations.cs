4d5708602006f31bd590d7f8de666870c9aa9d97
Cristhian Richard <criiss13.-@hotmail.com>
2017-02-06 17:11:42 -0300

    sigo

--- TP6/EJ2/AccountManager/Logic/ClientOperations.cs
+++ TP6/EJ2/AccountManager/Logic/ClientOperations.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccountManager.DAL;
 using AccountManager.DAL.EntityFramework;
 using AccountManager.Domain;
 using AccountManager.DTO;
+using AutoMapper;
 
 namespace AccountManager.Logic
 {
@@ -19,9 +21,42 @@ namespace AccountManager.Logic
             this.iUOW = pUnit;
         }
     
-        public void AltaClient (ClientDTO pClient)
+        public void CreateCliente (ClientDTO pClient)
         {
+            if (pClient.Id > -1)
+            {
+                throw new Exception("ID de cliente inválido");
+            }
 
+            if (pClient.DocumentType < 0)
+            {
+                throw new Exception("No se ha introducido un tipo de Documento");
+            }
+
+            if (String.IsNullOrWhiteSpace(pClient.DocumentNumber))
+            {
+                throw new Exception("No se ha introducido Documento de cliente");
+            }
+
+            if (String.IsNullOrWhiteSpace(pClient.FirstName))
+            {
+                throw new Exception("No se ha introducido Nombre de cliente");
+            }
+
+            if (String.IsNullOrWhiteSpace(pClient.LastName))
+            {
+                throw new Exception("No se ha introducido Nombre de cliente");
+            }
+            try
+            {
+                var cliente = Mapper.Map<Client>(pClient);
+                this.iUOW.ClientRepository.Add(cliente);
+                this.iUOW.Complete();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Se ha producido un error al guardar el cliente");
+            }
         }
     }
 }
