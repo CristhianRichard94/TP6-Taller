81ef274f38f41f4adc7e76dc88e27d0e62762c5d
Lia Lucia <Lia Lucia@DESKTOP-IDRU02F>
2017-02-06 19:06:39 -0300

    dasjkhdhjksahkds

---
+++ TP6/EJ2/AccountManager/Logic/Facade.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountManager.DAL.EntityFramework;
+
+namespace AccountManager.Logic
+{
+    class Facade
+    {
+        private ClientOperations iClient;
+        private AccountOperations iAccount;
+
+        public Facade(UnitOfWork pUOW)
+        {
+            this.iClient = new ClientOperations(pUOW);
+            this.iAccount = new AccountOperations(pUOW);
+        }
+
+        public ClientOperations Cliente { get { return this.iClient; } }
+        public AccountOperations Cuenta { get { return this.iAccount; } }
+    }
+}
