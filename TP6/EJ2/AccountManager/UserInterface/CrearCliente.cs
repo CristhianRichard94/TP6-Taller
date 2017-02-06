e96c8cc76571d4d49d990f361e8d6aeffa1a0a88
Cristhian Richard <criiss13.-@hotmail.com>
2017-02-06 19:47:14 -0300

    tumami

--- TP6/EJ2/AccountManager/UserInterface/CrearCliente.cs
+++ TP6/EJ2/AccountManager/UserInterface/CrearCliente.cs
@@ -7,14 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AccountManager.DTO;
+using AccountManager.Logic;
+using AccountManager.Domain;
 
 namespace AccountManager.UserInterface
 {
     public partial class CrearCliente : Form
     {
+        private ClientDTO iClient;
+        public Facade iFacade;
+
         public CrearCliente()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.iClient.FirstName = this.textBox1.Text;
+            this.iClient.LastName = this.textBox2.Text;
+            this.iClient.DocumentNumber = this.textBox3.Text;
+            this.iClient.DocumentType = (DocumentType)this.comboBox1.SelectedIndex;
+
+            if (this.iClient.Id > 0)
+            {
+                try
+                {
+                    iFacade.Cliente.UpdateClient(this.iClient);
+                    this.Close();
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Se ha producido un error\n" + exception.Message);
+                }
+            }
+            else
+            {
+                try
+                {
+                    iFacade.Cliente.CreateClient(this.iClient);
+                    this.Close();
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Se ha producido un error\n" + exception.Message);
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- TP6/EJ2/AccountManager/UserInterface/CrearCuenta.Designer.cs
+++ TP6/EJ2/AccountManager/UserInterface/CrearCuenta.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CrearCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 298);
+            this.Name = "CrearCuenta";
             this.Text = "CrearCuenta";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
